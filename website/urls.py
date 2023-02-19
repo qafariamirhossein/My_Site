2294baaf50406cc1483326d8c045f6fdfe269a8e
qafariamirhossein <qafariamirhossein@gmail.com>
2023-02-19 14:44:21 +0330

    Create
    +templates
    +statics
    edit settings.py
    +load static index
    edit views , urls

---
+++ website/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name = 'website'
+
+urlpatterns = [
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
+    path('blog/',blog_view,name='blog'),
+    path('post-details',postdetails_view,name='post-details'),
+
+]
---
+++ website/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index_view(request):
+    return render(request,'index.html')
+    
+def about_view(request):
+    return render(request,'about.html')
+    
+def contact_view(request):
+    return render(request,'contact.html')
+    
+def blog_view(request):
+    return render(request,'blog.html')
+
+def postdetails_view(request):
+    return render(request,'post-details.html')
