accabe05215d6575ab61ad0f6e8f1a406eb79e8f
qafariamirhossein <qafariamirhossein@gmail.com>
2023-02-23 14:49:47 +0330

    edit
    +all of the static files
    +base.html =>footer & header
    +models post

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to ='uploads/',default='image/banner-item-03.jpg')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    #category = models.ManyToManyField()
+    #tag
+    author = models.CharField(max_length=255)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    #comment
--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index_view(request):
-    return render(request,'index.html')
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request,'index.html',context)
     
 def about_view(request):
     return render(request,'about.html')
