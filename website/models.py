838370077a37f87518ecdac061d9ec3f49eed231
qafariamirhossein <qafariamirhossein@gmail.com>
2023-02-26 14:41:03 +0330

    Great
    fixed

--- website/models.py
+++ website/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 from taggit.managers import TaggableManager
-
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Post(models.Model):
     image = models.ImageField(upload_to ='website/',default='website/default.jpg')
+    writer = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     status = models.BooleanField(default=False)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    category = models.ManyToManyField(Category)
     tags = TaggableManager()
-    author = models.CharField(max_length=255)
     created_date = models.DateTimeField()
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(auto_now_add=True)
