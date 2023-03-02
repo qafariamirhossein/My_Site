1ac98b5f8e70399ffa423ad6fd5bc1f5bdb66ff6
qafariamirhossein <qafariamirhossein@gmail.com>
2023-03-02 18:06:02 +0330

    yeap!
    +sign-in
    +logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib.auth import logout
 
 def signup_view(request):
     return render(request,'accounts/sign-up.html')
 
 def signin_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')        
+
     return render(request,'accounts/sign-in.html')
 
 def logout_view(request):
-    return render(request,'accounts/logout.html')
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/')
